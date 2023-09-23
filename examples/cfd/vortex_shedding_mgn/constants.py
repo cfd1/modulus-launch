3b85fda52ef67a23e3cc2e6a9433a12006a2e67d
LimitingFactor <aswift0n3@gmail.com>
2023-09-23 14:25:24 +0100

    improved the inference plotting, fixed the checkpoint loading

--- examples/cfd/vortex_shedding_mgn/constants.py
+++ examples/cfd/vortex_shedding_mgn/constants.py
@@ -21,33 +21,57 @@ from typing import Tuple, Optional
 class Constants(BaseModel):
     """vortex shedding constants"""
 
+    # Model name
+    model_name: str = "test_2"
+
     # data configs
-    data_dir: str = "./raw_dataset/cylinder_flow/cylinder_flow"
+    data_dir: str = "/home/swifta/modulus/datasets/cylinder_flow/cylinder_flow"
 
     # training configs
     batch_size: int = 1
     epochs: int = 25
-    num_training_samples: int = 400
-    num_training_time_steps: int = 300
+    num_training_samples: int = 1000
+    num_training_time_steps: int = 600
+    training_noise_std: float = 0.02
+
+    num_valid_samples: int = 100
+    num_valid_time_steps: int = 600
+
     lr: float = 0.0001
     lr_decay_rate: float = 0.9999991
+    ckpt_path: str = "checkpoints_test_3"
+    ckpt_name: str = "test_3.pt"
+
+    # Mesh Graph Net Setup
     num_input_features: int = 6
-    num_output_features: int = 3
     num_edge_features: int = 3
-    ckpt_path: str = "checkpoints"
-    ckpt_name: str = "model.pt"
+    num_output_features: int = 3
+    processor_size: int = 15
+    num_layers_node_processor: int = 2
+    num_layers_edge_processor: int = 2
+    hidden_dim_processor: int = 128
+    hidden_dim_node_encoder: int = 128
+    num_layers_node_encoder: int = 2
+    hidden_dim_edge_encoder: int = 128
+    num_layers_edge_encoder: int = 2
+    hidden_dim_node_decoder: int = 128
+    num_layers_node_decoder: int = 2
+    aggregation: str = "sum"
+    do_concat_trick: bool = False
+    num_processor_checkpoint_segments: int = 0
+    activation_fn: str = "silu"
 
     # performance configs
     amp: bool = False
     jit: bool = False
 
     # test & visualization configs
-    num_test_samples: int = 10
-    num_test_time_steps: int = 300
+    num_test_samples: int = 100
+    num_test_time_steps: int = 600
     viz_vars: Tuple[str, ...] = ("u", "v", "p")
     frame_skip: int = 10
     frame_interval: int = 1
 
     # wb configs
-    wandb_mode: str = "disabled"
-    watch_model: bool = False
+    wandb_mode: str = "online"
+    watch_model: bool = True
